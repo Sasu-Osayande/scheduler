10becbf0cdaf6110ac2a8acc74ce192af293d20d
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-23 16:56:29 -0400

    Add the Empty component to the stories for Appointment

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,8 +140,9 @@ storiesOf("Appointment", module)
 
 storiesOf("Appointment", module)
   .addParameters({
-    backgrounds: [{ name: "white", value: "#fff", default: true }]
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
