613a5bfa8ddaac7b1f71e1a14b5140a3f954c0ec
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-23 09:51:00 -0400

    Refactor the component to map through the array and display it inside the <ul> tag

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
+  const interviewerListItems = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewerListItems}</ul>
     </section>
   );
 }
