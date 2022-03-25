37df86c66d86ade694fea4c31f659236324513fa
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-25 00:12:31 -0400

    Import axios and remove the days array to request the data from the GET request with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,68 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+useEffect(() => {
+  axios.get("/api/days").then(response => {
+    console.log(response.data);
+    setDays([response.data]);
+  });
+}, []);
 
 return (
   <main className="layout">
@@ -83,7 +35,7 @@ return (
       />
     </section>
     <section className="schedule">
-      {Object.values(appointments).map(appointment => <Appointment key={appointment.id} {...appointment} />)}
+      {/* {Object.values(appointments).map(appointment => <Appointment key={appointment.id} {...appointment} />)} */}
       <Appointment key="last" time="5pm" />
     </section>
   </main>
