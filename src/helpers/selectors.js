18120a73b7b632557f537ac0413a5145e0b32cf4
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-25 16:39:31 -0400

    Write logic for the getInterview function to return null if there is no interview, otherwise return the interview data object

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,9 @@
-import DayList from "components/DayList";
-
 export function getAppointmentsForDay(state, day) {
   let appointmentsArray = [];
 
-  const daysAppointmentsArray = state.days.filter(dayValue => dayValue.name === day);
+  const daysAppointmentsArray = state.days.filter(
+    (dayValue) => dayValue.name === day
+  );
 
   if (state.days.length === 0 || daysAppointmentsArray.length === 0) {
     return appointmentsArray;
@@ -15,4 +15,22 @@ export function getAppointmentsForDay(state, day) {
     appointmentsArray.push(state.appointments[appointment]);
   }
   return appointmentsArray;
+}
+
+export function getInterview(state, interview) {
+  // let interviewObj = {};
+  if (!interview) {
+    return null;
+  }
+  return {
+      student: interview.student,
+      interviewer: {
+        id: interview.interviewer,
+        name: state.interviewers[interview.interviewer].name,
+        avatar: state.interviewers[interview.interviewer].avatar
+      }
+  }
+  // if (Object.keys(interviewObj).length === 0) {
+  //   Object.assign(interviewObj, { "interviewer": state.interviewers[interview.interviewer], "student": interview.stdent});
+  // }
 }
