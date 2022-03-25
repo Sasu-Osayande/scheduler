0b92b895a4c38a773af7d6fae9ba481a3620c84d
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-25 01:43:38 -0400

    Correct error with deleted appointments data

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,45 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment"
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   const [days, setDays] = useState([]);
@@ -35,7 +74,7 @@ return (
       />
     </section>
     <section className="schedule">
-      {/* {Object.values(appointments).map(appointment => <Appointment key={appointment.id} {...appointment} />)} */}
+      {Object.values(appointments).map((appointment) => <Appointment key={appointment.id} {...appointment} />)}
       <Appointment key="last" time="5pm" />
     </section>
   </main>
