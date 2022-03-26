2625cad9d1e6ec5b97e88ce5b3948b0d17314a6a
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-25 20:33:04 -0400

    Incorporate useVisualMode and add mode constants to render components and handle transitions

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,21 +4,36 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <Fragment>
       <article className="appointment">
         <Header time={props.time} />
-        {props.interview ? 
-        <>
-        <Show student={props.interview.student} interviewer={props.interview.interviewer}/> 
-        </>
-        :
-        <>
-        <Empty />
-        </>
-        }
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+        {mode === CREATE && (
+          <Form
+          interviewers={[]}
+          onSave={("onSave")}
+          onCancel={back}
+          />
+        )}
       </article>
     </Fragment>
   );
