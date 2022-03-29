b5f8631dc0e9525df2fa859168d91079b1ec072a
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-29 09:33:35 -0400

    In the bookInterview object, work through levels to update the appointments object and update the new state

--- src/components/Application.js
+++ src/components/Application.js
@@ -42,6 +42,21 @@ export default function Application(props) {
 
   function bookInterview(id, interview) {
     console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({
+      ...state,
+      appointments
+    });
   }
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
