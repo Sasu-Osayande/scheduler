fabfecd3e81b346dda258c0cdf00703cc9bcc36c
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-30 21:41:44 -0400

    Add testing for cypress

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,11 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("Should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
+  })
+});
