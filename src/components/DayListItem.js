ee4aedd7b3ab48d0a79dcbf912da5815d516b6cd
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-30 22:03:50 -0400

    Add code to satisfy the cypress navigation test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
